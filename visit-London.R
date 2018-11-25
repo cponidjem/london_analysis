346308ed2aa932a5d7a17c70d4e9437cc193a2d3
Cindy Ponidjem <cindy.ponidjem@outlook.fr>
2018-11-25 16:13:09 +0100

    Try to correlate tourist affluence to crime in London

--- visit-London.R
+++ visit-London.R
@@ -7,6 +7,7 @@
 
 library(ggplot2)
 library(plyr)
+library(dplyr)
 library(gdata)
 library(readxl)
 
@@ -15,5 +16,43 @@ View(visits)
 
 perCountry <- ddply(visits, .(market), summarise, sum=sum(sample))
 topCountry <- head(arrange(perCountry, -sum),10)
-topNameF <- head(arrange(nameF, -sum),5)
+#topNameF <- head(arrange(nameF, -sum),5)
 ggplot(topCountry, aes(x=market, y=sum, fill=market))+geom_bar(stat="identity",position="dodge")
+
+
+## Chercher une correlation entre le nombre de crimes et l'afluence de touristes a Londres 
+
+# Afluence de touristes par quart d'annee de 2008 à 2016
+perQuarter <- ddply(visits, .(year,quarter),summarise, sum=sum(sample))
+perQuarter2008_2016 <- subset(perQuarter,(year>=2008&year<=2016))
+p <- ggplot(perQuarter2008_2016, aes(x=factor(year), y=sum, fill=quarter))+geom_bar(stat="identity",position="dodge")
+p+labs(title="Afluence de touristes par quart d'année \n de 2008 à 2016",x ="Année", y = "Nombre de touristes")
+
+# Recuperer le dataset sur les crimes a Londres
+crimes <- read.table(file="london_crime_by_lsoa.csv",header=TRUE,sep=",")
+# Traiter les donnees sur le crime pour qu'elles soient comparables avec les données sur l'affluence : ajout de la colonne quarter
+getQuarter <- function(x) {
+  case_when(
+    x >= 1 & x <= 3 ~ "Q1",
+    x >= 4 & x <= 6 ~ "Q2",
+    x >= 7 & x <= 9 ~ "Q3",
+    x >= 10 & x <= 12 ~ "Q4"
+  )
+}
+crimes <- cbind(crimes,quarter=getQuarter(crimes$month))
+
+# Decommenter la ligne suivante pour voir les grandes categories de crime
+#levels(factor(crimes$major_category))
+
+# Nombres de vols par quart d'annee de 2008 a 2016
+theft <- subset(crimes,(major_category=="Burglary"|major_category=="Robbery"|major_category=="Theft and Handling"),select=c(major_category,value,year,quarter))
+theftPerQuarter <- ddply(theft, .(year,quarter), summarise, sum=sum(value))
+p <- ggplot(theftPerQuarter, aes(x=factor(year), y=sum, fill=quarter))+geom_bar(stat="identity",position="dodge")
+p+labs(title="Nombres de vols par quart d'année \n de 2008 à 2016",x ="Année", y = "Nombre de vols")
+
+# Nombres de violences contre la personne de 2008 a 2016
+violenceAgainstPerson <- subset(crimes,(major_category=="Violence Against the Person"|major_category=="Sexual Offences"),select=c(major_category,value,year,quarter))
+violenceAgainstPersonPerQuarter <- ddply(violenceAgainstPerson, .(year,quarter), summarise, sum=sum(value))
+p <- ggplot(violenceAgainstPersonPerQuarter, aes(x=factor(year), y=sum, fill=quarter))+geom_bar(stat="identity",position="dodge")
+p+labs(title="Nombres de cas violences contre la personne \n par quart d'année  de 2008 à 2016",x ="Année", y = "Nombre de cas")
+
